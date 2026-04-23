a3e1a662786cb691d20c6ec9e209d5fd8aaf8c0b
ivobarrera <143567603+ivobarrera@users.noreply.github.com>
2026-04-22 21:32:35 -0300

    asdasd

--- hola.py
+++ hola.py
@@ -2,8 +2,12 @@ print("Hola mundo desde prueba22.")
 
 nombre = input("Ingresa tu nombre: ")
 
+<<<<<<< Updated upstream
 numero1 = int(input("Ingresa el primer numero: "))
 
 numero2 = int(input("Ingresa el segundo numero: "))
 
 print(f"La suma es: {numero1 + numero2}")
+=======
+print(f"Hola {nombre}")
+>>>>>>> Stashed changes
